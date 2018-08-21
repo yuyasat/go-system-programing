599083078c292f5fecb18b53b3135ba89780b851
yuyasat <yuya.taki@enechange.co.jp>
2018-08-21 19:24:25 +0900

    2018-08-21

---
+++ 8/8-2-4-2.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	clientPath := filepath.Join(os.TempDir(), "unixdomainsocket-client")
+	os.Remove(clientPath)
+	conn, err := net.ListenPacket("unixgram", clientPath)
+	if err != nil {
+		panic(err)
+	}
+	unixServerAddr, err := net.ResolveUnixAddr(
+		"unixgram", filepath.Join(os.TempDir(), "unixdomainsocket-server"))
+	var serverAddr net.Addr = unixServerAddr
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	log.Println("Sending to server")
+	_, err = conn.WriteTo([]byte("Hello from Client"), serverAddr)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Receiving from server")
+	buffer := make([]byte, 1500)
+	length, _, err := conn.ReadFrom(buffer)
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("Recevied %s\n", string(buffer[:length]))
+}
